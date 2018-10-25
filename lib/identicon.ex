d7cbf6aaff357427b5d6ba6281699f5f8244ae4b
ValdoR Che <ingeekteam@gmail.com>
2018-10-25 18:59:44 +0100

    Constructing of grid. With Enum.chunk_every instead of Enum.chunk

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -20,6 +20,26 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = _image) do
+    hex
+    |> Enum.chunk_every(3)
+    |> mirror_row
+  end
+
+  @doc """
+    Mirroring our 3 elements
+  ## Examples
+      iex> image = [[3 , 2 , 8],[1, 2, 3]]
+      iex> Identicon.mirror_row(image)
+            [[3 , 2 , 8 , 2 , 3], [1, 2, 3, 2 , 1]]
+  """
+  def mirror_row(image) do
+    for [first, second | _other] = row <- image do
+      List.flatten([row | [second, first]])
+    end
   end
 
   def hash_input(input) do
